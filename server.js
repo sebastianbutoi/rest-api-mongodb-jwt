543ecd117ebe01f9302a45195214ef650b496695
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-03-01 00:49:00 +0000

    json middleware

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
