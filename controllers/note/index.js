10b19b5a8daba95dd01f6e8f548514a72d542b1d
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-03-03 00:14:06 +0000

    Added update note function

--- controllers/note/index.js
+++ controllers/note/index.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import Note from "../../models/note";
+import User from "../../models/user";
 
 export const getNotes = asyncHandler(async (req, res) => {
   const notes = await Note.find({ user: req.user.id });
@@ -27,7 +28,31 @@ export const setNote = asyncHandler(async (req, res) => {
 });
 
 export const updateNote = asyncHandler(async (req, res) => {
-  //
+  const id = req.params.id;
+  const note = await Note.findById(id);
+  if (!note) {
+    res.status(400);
+    throw new Error("Note not found.");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found.");
+  }
+
+  if (note.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized.");
+  }
+
+  const updatedNote = await Note.findByIdAndUpdate(id, req.body, { new: true });
+
+  res.status(200).json({
+    status: "success",
+    payload: updatedNote,
+  });
 });
 
 export const deleteNote = asyncHandler(async (req, res) => {
