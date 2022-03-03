d98ebeabe8fa97c8f6ce5ce6f5b1648597129b14
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-03-03 00:45:56 +0000

    Added routers

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import notesRouter from "./routes/note/index.js";
+import userRouter from "./routes/user/index.js";
+import { errorHandler } from "./middleware/error";
 
 dotenv.config();
 
@@ -10,6 +13,10 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/notes", notesRouter);
+app.use("/api/users", userRouter);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
