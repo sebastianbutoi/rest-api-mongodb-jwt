7edf4adb57bac849f8840b858b57798153b6f9dc
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-03-03 00:03:33 +0000

    Added async handler

--- controllers/note/index.js
+++ controllers/note/index.js
@@ -1,15 +1,17 @@
-export const getNotes = async (req, res) => {
-    //
-}
+import asyncHandler from "express-async-handler";
 
-export const setNote = async (req, res) => {
-    //
-}
+export const getNotes = asyncHandler(async (req, res) => {
+  //
+});
 
-export const updateNote = async (req, res) => {
-    //
-}
+export const setNote = asyncHandler(async (req, res) => {
+  //
+});
 
-export const deleteNote = async (req, res) => {
-    //
-}
+export const updateNote = asyncHandler(async (req, res) => {
+  //
+});
+
+export const deleteNote = asyncHandler(async (req, res) => {
+  //
+});
