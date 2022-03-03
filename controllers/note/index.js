d87db73c9eb39a8932f421e1c8353fc6271f1092
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-03-03 00:20:55 +0000

    Added comments

--- controllers/note/index.js
+++ controllers/note/index.js
@@ -17,11 +17,13 @@ export const getNotes = asyncHandler(async (req, res) => {
 // @route   POST /api/notes
 // @access  Private
 export const setNote = asyncHandler(async (req, res) => {
+  // Check if the text field is missing
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text field");
   }
 
+  // Create a new note
   const newNote = await Note.create({
     text: req.body.text,
     user: req.user.id,
@@ -38,26 +40,27 @@ export const setNote = asyncHandler(async (req, res) => {
 // @access  Private
 export const updateNote = asyncHandler(async (req, res) => {
   const id = req.params.id;
+  // Search the note
   const note = await Note.findById(id);
   if (!note) {
     res.status(400);
     throw new Error("Note not found.");
   }
 
+  // Check the user
   const user = await User.findById(req.user.id);
-
   if (!user) {
     res.status(401);
     throw new Error("User not found.");
   }
 
+  // Make sure the logged in user matches the note user
   if (note.user.toString() !== user.id) {
     res.status(401);
     throw new Error("User not authorized.");
   }
 
   const updatedNote = await Note.findByIdAndUpdate(id, req.body, { new: true });
-
   res.status(200).json({
     status: "success",
     payload: updatedNote,
@@ -69,19 +72,22 @@ export const updateNote = asyncHandler(async (req, res) => {
 // @access  Private
 export const deleteNote = asyncHandler(async (req, res) => {
   const id = req.params.id;
-  const note = await Note.findById(id);
 
+  // Search the note
+  const note = await Note.findById(id);
   if (!note) {
     res.status(400);
     throw new Error("Note not found");
   }
   const user = await User.findById(req.user.id);
 
+  // Check the user
   if (!user) {
     res.status(401);
     throw new Error("User not found.");
   }
 
+  // Make sure the logged in user matches the note user
   if (note.user.toString() !== user.id) {
     res.status(401);
     throw new Error("User not authorized.");
