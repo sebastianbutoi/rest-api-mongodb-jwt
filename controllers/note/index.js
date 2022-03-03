6eb606e25a5640d91ce0c2dc5f09a2f2a34ae87d
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-03-03 00:02:46 +0000

    crud operations

---
+++ controllers/note/index.js
@@ -0,0 +1,15 @@
+export const getNotes = async (req, res) => {
+    //
+}
+
+export const setNote = async (req, res) => {
+    //
+}
+
+export const updateNote = async (req, res) => {
+    //
+}
+
+export const deleteNote = async (req, res) => {
+    //
+}
