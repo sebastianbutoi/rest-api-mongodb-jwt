fc419cb40c0de255b0542d5ddce31f46219deaca
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-03-03 00:41:55 +0000

    added jwt check to the routes

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { getUser, loginUser, registerUser } from "../../controllers/user";
+import { checkToken } from "../../middleware/auth";
 
 const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
-router.get("/me", getUser);
+router.get("/me", checkToken, getUser);
 
 export default router;
