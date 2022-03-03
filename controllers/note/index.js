7aa7d33c2abcc763a923f292b7c89e50dc130890
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-03-03 00:04:38 +0000

    Added getnotes function

--- controllers/note/index.js
+++ controllers/note/index.js
@@ -1,7 +1,12 @@
 import asyncHandler from "express-async-handler";
+import Note from "../../models/note";
 
 export const getNotes = asyncHandler(async (req, res) => {
-  //
+  const notes = await Note.find({ user: req.user.id });
+  res.status(200).json({
+    status: "success",
+    payload: notes,
+  });
 });
 
 export const setNote = asyncHandler(async (req, res) => {
