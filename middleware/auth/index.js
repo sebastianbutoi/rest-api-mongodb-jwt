224a57c7cc3607e16b7a7ca95402948acaf00c9e
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-03-03 00:36:36 +0000

    Added comments

--- middleware/auth/index.js
+++ middleware/auth/index.js
@@ -3,13 +3,17 @@ import User from "../../models/user";
 import asyncHandler from "express-async-handler";
 
 export const checkToken = asyncHandler(async (req, res, next) => {
+  // Check if the token is not missing
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     try {
+      // Get token from header
       const token = req.headers.authorization.split(" ")[1];
+      // Verify token
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // Get user from the token - do not select the password field
       req.user = await User.findById(decoded.id).select("-password");
       next();
     } catch (error) {
